5d6c4c284e0e0274a565843632971525bf21d5c4
Ryan Sosinski <sosinskirv@gmail.com>
2022-11-26 16:34:32 -0500

    api endpoint commit

--- GroupIS1024/ChefAuthor.cs
+++ GroupIS1024/ChefAuthor.cs
@@ -1,6 +1,50 @@
 ﻿namespace GroupIS1024
 {
-    public class ChefAuthor
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class ChefAuthor
     {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("favoriteRecipe")]
+        public string FavoriteRecipe { get; set; }
+    }
+
+    public partial class ChefAuthor
+    {
+        public static List<ChefAuthor> FromJson(string json) => JsonConvert.DeserializeObject<List<ChefAuthor>>(json, GroupIS1024.Converter2.Settings);
+    }
+
+    public static class Serialize2
+    {
+        public static string ToJson(this List<ChefAuthor> self) => JsonConvert.SerializeObject(self, GroupIS1024.Converter2.Settings);
+    }
+
+    internal static class Converter2
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
     }
 }
